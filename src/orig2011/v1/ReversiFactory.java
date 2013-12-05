9be83cede88f8ff0c4d0d676e5b8cfd7c71d57b5
Axel Niklasson <axel.niklasson@live.com>
2013-12-05 13:39:43 +0100

    Ändrade tillbaka igen. Verkar fungera det här!

--- src/orig2011/v1/ReversiFactory.java
+++ src/orig2011/v1/ReversiFactory.java
@@ -4,8 +4,6 @@ package orig2011.v1;
  * Factory class for available games.
  */
 public class ReversiFactory implements IGameFactory {
-	
-	//Andrar bara lite 
 
 	/**
 	 * Returns an array with names of games this factory can create. Used by GUI
--- src/orig2011/v2/GameUtils.java
+++ src/orig2011/v2/GameUtils.java
@@ -1,5 +1,7 @@
 package orig2011.v2;
 
 public class GameUtils {
+	
+	//Ska vara 3/4 metoder här inne
 
 }
